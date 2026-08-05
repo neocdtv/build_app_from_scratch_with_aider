07e828126b7ed5219645e1cc340cafe6156d1aaf
krzysztof.wolf <krzysztof.wolf@emeal.nttdata.com>
2026-08-05 15:25:22 +0200

    update

--- src/main/java/com/example/addressbook/model/dto/ContactDto.java
+++
@@ -1,6 +0,0 @@
-package com.example.addressbook.model.dto;
-import java.time.LocalDateTime;
-
-public record ContactDto(Long id, String firstName, String lastName, String email, 
-                          String phoneNumber, String address, String category, 
-                          LocalDateTime createdAt, LocalDateTime updatedAt) {}
