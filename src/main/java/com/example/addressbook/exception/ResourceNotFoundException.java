833b9879cbf5f3235d45c6c2259ce891d62c7e82
krzysztof.wolf <krzysztof.wolf@emeal.nttdata.com>
2026-08-05 15:48:59 +0200

    update

--- src/main/java/com/example/addressbook/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.addressbook.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(Long id) {
-        super("Contact not found: " + id);
-    }
-}
