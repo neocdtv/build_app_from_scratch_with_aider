8785a031d57967acd260a9fc2f25d662e3aba845
krzysztof.wolf <krzysztof.wolf@emeal.nttdata.com>
2026-08-06 12:23:15 +0200

    update

--- src/main/java/com/addressbook/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.addressbook.repository;
-
-import com.addressbook.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
