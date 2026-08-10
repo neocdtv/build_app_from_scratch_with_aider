0e36fd9315e0adb1cfb6a88e94cf6944f80de928
krzysztof.wolf <krzysztof.wolf@emeal.nttdata.com>
2026-08-10 16:24:25 +0200

    update

--- src/main/java/com/addressbook/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.addressbook.repository;
-
-import com.addressbook.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-    List<Contact> findByFirstNameContainingOrLastNameContainingOrCategoryContaining(String firstName, String lastName, String category);
-}
